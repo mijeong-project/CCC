2fea2172d0af1a0edf72e5b59a3cbdb2fcca2515
mijeong-project <alwjd6405@naver.com>
2024-10-29 16:54:16 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -8,6 +8,28 @@ var swiper = new Swiper('.flowslide_inner ', {
         disableOnInteraction: false,
     },
     loop: true,//슬라이드 무한반복
+    breakpoints: {
+        1000: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+        460: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        370: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    },
 });
 
 $(function () {
@@ -23,10 +45,10 @@ $(function () {
 $(function () {
     var executed2 = false;
 
-    $('.section2').waypoint(function () {
+    $('.story').waypoint(function () {
         var targetNumber = $('.animate2').attr('data-rate');
         if (!executed2) {
-            var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',') 
+            var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
             $('.animate2').animateNumber({
                 number: targetNumber,
                 numberStep: comma_separator_number_step
@@ -42,8 +64,8 @@ $(function () {
 });
 
 // 형광펜 애니메이션
-  // IntersectionObserver 를 등록한다.
-  const io = new IntersectionObserver(entries => {
+// IntersectionObserver 를 등록한다.
+const io = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         // 관찰 대상이 viewport 안에 들어온 경우 'on' 클래스를 추가
         if (entry.intersectionRatio > 0) {
@@ -69,13 +91,76 @@ AOS.init({
 });
 
 // 슬라이드 내용소개
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
+var swiper2 = new Swiper(".mySwiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
     mousewheel: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
+    breakpoints: {
+        740: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    },
+    watchOverflow : true,
+    on: {
+        slideChange: function () {
+            setTimeout(function () {
+                swiper2.params.touchReleaseOnEdges = false;
+                swiper2.params.mousewheel.releaseOnEdges = false;
+            });
+        },
+        reachEnd: function () {
+            setTimeout(function () {
+                swiper2.params.touchReleaseOnEdges = true;
+                swiper2.params.mousewheel.releaseOnEdges = true;
+            }, 500);
+        },
+        reachBeginning: function () {
+            setTimeout(function () {
+                swiper2.params.touchReleaseOnEdges = true;
+                swiper2.params.mousewheel.releaseOnEdges = true;
+            }, 500);
+        }
+    }
+
+});
+// 360도 회전
+const $carousel_cells = $('#product-carousel li');
+const $carousel_dots = $('#carousel-dots li');
+const $carousel_nav = $('.carousel-nav');
+let selected_product_index = 0;
+
+function selectProduct(index) {
+  selected_product_index = index % $carousel_cells.length;
+
+  if (selected_product_index < 0) selected_product_index = $carousel_cells.length + selected_product_index;
+
+  $carousel_cells.each(function (i) {
+    let offset = i - selected_product_index;
+    if (offset < 0) offset += $carousel_cells.length;
+
+    let index;
+    for (index = 0; index < $carousel_cells.length + 1; index++) {
+      $(this).removeClass('item-' + index).addClass('item-' + (offset + 1));
+    }
   });
 
+  $carousel_dots.eq(index).addClass('active').siblings('li').removeClass('active');
+}
+
+/* Arrow clicks */
+$carousel_nav.click(function () {
+  const delta = $(this).hasClass('prev') ? -1 : 1;
+  const $delta_product = $(`#product-carousel li:eq(${(selected_product_index + delta) % $carousel_cells.length})`);
+  const delta_product_index = parseInt($delta_product.index());
+  selectProduct(delta_product_index);
+});
+
+/* Can clicks */
+$carousel_cells.click(function () {
+  selectProduct($(this).index());
+});
